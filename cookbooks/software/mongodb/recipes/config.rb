ea55619238b4edc3e6aa2ac728518361c89325ac
Manuel Wiedenmann <manuel@funkensturm.de>
2014-06-09 15:08:55 +0200

    [mongodb] log into /mnt/log/mongodb/mongodb.log

--- cookbooks/software/mongodb/recipes/config.rb
+++ cookbooks/software/mongodb/recipes/config.rb
@@ -1,26 +1,27 @@
-username    = 'mongodb'
-config_path = Pathname.new '/etc'
-data_path   = Pathname.new node[:central][:data] + '/mongodb'
-log_path    = Pathname.new node[:central][:log] + '/mongodb'
+name          = 'mongodb'
+config_path   = Pathname.new '/etc'
+data_path     = Pathname.new node[:central][:data] + '/mongodb'
+log_path      = Pathname.new node[:central][:log] + '/mongodb'
+logfile_path  = ::File.join(log_path, 'mongodb.log')
 
 log 'Ensuring MongoDB database directory...'
 directory data_path.to_s do
-  owner username
-  group username
+  owner name
+  group name
   mode '0755'
 end
 
 log 'Ensuring MongoDB log directory...'
 directory log_path.to_s do
-  owner username
-  group username
+  owner name
+  group name
   mode '0755'
 end
 
 log 'Ensuring persmissions for the MongoDB log file...'
 file log_path.join('upstart.log').to_s do
-  owner username
-  group username
+  owner name
+  group name
   mode '0644'
   action :touch
 end
@@ -29,11 +30,12 @@ end
 log 'Configuring MongoDB...'
 template config_path.join('mongodb.conf').to_s do
   source "mongodb.conf.erb"
-  owner username
-  group username
+  owner name
+  group name
   mode '0644'
   variables({
-    data_path: data_path,
+    data_path:    data_path,
+    logfile_path: logfile_path,
   })
   notifies :restart, 'service[mongodb]'
 end
