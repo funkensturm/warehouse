e68f11cc194177ca0604f0cd30370eacd5584f62
Manuel Wiedenmann <manuel@funkensturm.de>
2014-07-25 19:51:33 +0200

    [mysql] fix fixed version

--- cookbooks/providers/parcel/attributes/default.rb
+++ cookbooks/providers/parcel/attributes/default.rb
@@ -3,5 +3,5 @@ default[:parcel][:versions] = {}
 # Here you could specify specific versions of apt-get packages.
 
 #default[:parcel][:versions]['git-core']     = '1:1.7.9.5-1'
-default[:parcel][:versions]['mysql-server'] = '5.5.37-0ubuntu0.14.04.1'
+default[:parcel][:versions]['mysql-server'] = '5.5.38-0ubuntu0.14.04.1'
 default[:parcel][:versions]['redis-server'] = '2:2.8.4-2'
