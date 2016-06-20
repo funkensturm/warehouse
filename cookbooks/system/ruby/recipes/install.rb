ab715aa3639604c70a74a7bbe8c2bdf2b1a0e1b6
Manuel Wiedenmann <manuel@funkensturm.de>
2016-06-20 22:31:51 +0200

    [ruby] update to ruby2.2 (#12)

--- cookbooks/system/ruby/recipes/install.rb
+++ cookbooks/system/ruby/recipes/install.rb
@@ -1,22 +1,35 @@
-parcel 'ruby2.0'
-parcel 'ruby2.0-dev'
+apt_repository 'ruby' do
+  uri          'http://ppa.launchpad.net/brightbox/ruby-ng/ubuntu'
+  distribution node['lsb']['codename']
+  components   ['main']
+  keyserver    'keyserver.ubuntu.com'
+  key          'C3173AA6'
+  deb_src      true
+end
 
-log 'Removing ruby1.9 symlinks and linking ruby2.0 bins'
+execute "apt-get update" do
+  user "root"
+end
+
+parcel 'ruby2.2'
+parcel 'ruby2.2-dev'
+
+log 'Removing ruby1.9 symlinks and linking ruby2.2 bins'
 
 %w{ ruby gem irb rdoc erb }.each do |file|
   link "/usr/bin/#{file}" do
     action :delete
   end
-  
+
   link "/usr/bin/#{file}" do
-    to "/usr/bin/#{file}2.0"
+    to "/etc/alternatives/#{file}"
     action :create
   end
 end
 
 log 'Linking config.h'
 
-link '/usr/include/ruby-2.0.0/ruby/config.h' do
-  to '/usr/include/x86_64-linux-gnu/ruby-2.0.0/ruby/config.h'
+link '/usr/include/ruby-2.2.0/ruby/config.h' do
+  to '/usr/include/x86_64-linux-gnu/ruby-2.2.0/ruby/config.h'
   action :create
 end
